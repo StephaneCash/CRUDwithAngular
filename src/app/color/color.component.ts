756fea38ca25fa42966a43d0d0f672935f671f84
Stephane_1 <kikonistephane@gmail.com>
2021-09-02 16:33:40 +0100

    11fin

--- src/app/color/color.component.ts
+++ src/app/color/color.component.ts
@@ -3,14 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-color',
   templateUrl: './color.component.html',
-  styleUrls: ['./color.component.css']
+  styleUrls: ['./color.component.css'],
 })
 export class ColorComponent implements OnInit {
+
   color = "red";
-  
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  processReq(message: any) {
+    console.log(message)
+  }
 }
--- src/app/fils-component/fils-component.component.ts
+++ src/app/fils-component/fils-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-fils-component',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fils-component.component.css']
 })
 export class FilsComponentComponent implements OnInit {
-
+  @Input() nameColor: any;
+  @Output() sendRequestData = new EventEmitter();
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): any {
+    console.log("La couleur héritée est : ", this.nameColor);
+  };
+
+  value = {
+    id: 1,
+    nom: "Stéphane",
+    titre:"FullStack"
   }
 
+  sendEvent() {
+    this.sendRequestData.emit(
+      this.value
+    );
+  }
 }
