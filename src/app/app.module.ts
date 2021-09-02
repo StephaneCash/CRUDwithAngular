120b0cf646749c0c81ce3acd2b1fce9210530574
Stephane_1 <kikonistephane@gmail.com>
2021-09-02 11:42:10 +0100

    d

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { StephComponent } from './steph/steph.component';
-import { CashComponent } from './cash/cash.component';
-import { KikoniComponent } from './kikoni/kikoni.component';
 import { ColorComponent } from './color/color.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StephComponent,
-    CashComponent,
-    KikoniComponent,
     ColorComponent
   ],
   imports: [
