7d92574816936cd0de0dcd0d574ad77a13f99c55
Stephane_1 <kikonistephane@gmail.com>
2021-09-02 10:00:22 +0100

    premierspas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StephComponent } from './steph/steph.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StephComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/steph/steph.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-steph',
+  templateUrl: './steph.component.html',
+  styleUrls: ['./steph.component.css']
+})
+export class StephComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
