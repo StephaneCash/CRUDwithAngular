3d871d43be4e9c8a42d4cd50ea68e7f5b7c9a7eb
Stephane_1 <kikonistephane@gmail.com>
2021-09-03 09:54:42 +0100

    cvComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { FormsModule} from "@angular/forms"
 import { AppComponent } from './app.component';
 import { ColorComponent } from './color/color.component';
 import { FilsComponentComponent } from './fils-component/fils-component.component';
+import { CvComponent } from './cvTech/cv/cv.component';
+import { ListeCvComponent } from './cvTech/liste-cv/liste-cv.component';
+import { ItemCvComponent } from './cvTech/item-cv/item-cv.component';
+import { DetailCvComponent } from './cvTech/detail-cv/detail-cv.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ColorComponent,
-    FilsComponentComponent
+    FilsComponentComponent,
+    CvComponent,
+    ListeCvComponent,
+    ItemCvComponent,
+    DetailCvComponent
   ],
   imports: [
     BrowserModule,
